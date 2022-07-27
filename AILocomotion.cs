ade581b45542a654f949cdebc3787e2c25c30ebe
nathanhyao <nathanhyao@gmail.com>
2022-07-27 02:29:42 -0400

    basic attack working with stopping distance

--- AILocomotion.cs
+++ AILocomotion.cs
@@ -9,15 +9,16 @@ public class AILocomotion : MonoBehaviour
     Animator animator;
 
     public Transform playerTransform;
-    private bool isClose; // player within maxRadius
+    private bool isClose; // player is within stoppingDistance
 
     public float maxTime = 1.0f;
+    [Tooltip("Will not recalculate destination until farther than maxDistance.")]
     public float maxDistance = 1.0f;
     private float timer = 0.0f;
 
     [Header("Attack")]
-    public float maxRadius;
-    public float minRadius;
+    public float stoppingDistance;
+    [Range(2.5f, 5)] public float size;
 
     // Start is called before the first frame update
     void Start()
@@ -37,16 +38,30 @@ public class AILocomotion : MonoBehaviour
         {
             Vector3 playerToAgent = transform.position - playerTransform.position;
 
-            if (playerToAgent.sqrMagnitude < maxRadius * maxRadius)
-                isClose = true;
-            else
-                isClose = false;
+            if (playerToAgent.sqrMagnitude < Mathf.Pow(stoppingDistance, 2))
+            {
+                agent.isStopped = true;
+            }
+            else // todo: check that stomp has finished
+            {
+                agent.isStopped = false;
+            }
 
-            Vector3 destination = playerTransform.position + playerToAgent.normalized * maxRadius;
-            float sqDistance = (transform.position - destination).sqrMagnitude;
-            if (!isClose && sqDistance > maxDistance * maxDistance)
+            Vector3 destination = playerTransform.position + playerToAgent.normalized * stoppingDistance;
+            float sqDistance = (transform.position - playerTransform.position).sqrMagnitude;
+            if (!agent.isStopped && sqDistance > maxDistance * maxDistance)
             {
                 agent.SetDestination(destination);
+            }
+
+            if (playerToAgent.sqrMagnitude < Mathf.Pow(stoppingDistance + size, 2) &&
+            playerToAgent.sqrMagnitude > Mathf.Pow(stoppingDistance - size, 2))
+            {
+                animator.SetBool("atRadius", true);
+            }
+            else
+            {
+                animator.SetBool("atRadius", false);
             }
 
             timer = maxTime;
